cce8eff7ebf1b3904b2c8c6cb48b8deeacd7dbf5
Dan Fritchman <dan@fritch.mn>
2022-11-30 12:19:31 -0800

    Initial schematic instance, port, and connection parsing

--- export.py
+++ export.py
@@ -1,6 +1,6 @@
 import bag  # stick this right at the top so we're sure it works
-import inspect, os, sys, importlib
-import json
+import inspect, os, sys, importlib, json
+from enum import Enum
 from pathlib import Path
 from dataclasses import field
 from types import ModuleType
@@ -27,6 +27,46 @@ class BagSchematicInstance:
     xform: Any
 
 
+class SchematicPinDir(Enum):
+    INPUT = "ipin"
+    OUTPUT = "opin"
+    INOUT = "iopin"
+
+
+@dataclass
+class BagSchematicTerminalInner:
+    """# BAG Schematic Terminal (Port)
+    Inner Data"""
+
+    inst: BagSchematicInstance
+    attr: Dict[str, Any]
+
+    @property
+    def direction(self) -> SchematicPinDir:
+        if self.inst.cell_name == "ipin":
+            return SchematicPinDir.INPUT
+        if self.inst.cell_name == "opin":
+            return SchematicPinDir.OUTPUT
+        if self.inst.cell_name == "iopin":
+            return SchematicPinDir.INOUT
+        raise ValueError(f"Invalid terminal pin cell_name {self.inst.cell_name}")
+
+
+@dataclass
+class BagSchematicTerminal:
+    """# BAG Schematic Terminal (Port) Schema
+    Most relevant data lies on the `Inner` internal attribute."""
+
+    obj: Tuple[int, BagSchematicTerminalInner]
+    stype: int
+    ttype: int
+
+    @property
+    def inner(self) -> BagSchematicTerminalInner:
+        # Get our `Inner` definition element
+        return self.obj[1]
+
+
 @dataclass
 class BagSchematic:
     """#  BAG Schematic Schema"""
@@ -37,7 +77,7 @@ class BagSchematic:
     view_name: str
 
     # Core schematic content
-    terminals: Dict[str, Any]  # FIXME: more specifics
+    terminals: Dict[str, BagSchematicTerminal]
     instances: Dict[str, BagSchematicInstance]
 
     # Unused Fields
@@ -72,15 +112,6 @@ class SchematicGenerator:
     source_paths: SourcePaths
     pymodule: Any  # really a python-module, pydantic doesn't really like em
     sch: BagSchematic
-
-
-@dataclass(frozen=True)
-class SchematicGeneratorPaths:
-    # Turns out loading this YAML takes FOREVER, so we'll cache these things on disk
-    lib_name: str
-    cell_name: str
-    module_path: Path
-    sch_path: Path
 
 
 @dataclass
@@ -201,9 +232,7 @@ def try_candidate(source_paths: SourcePaths) -> Optional[SchematicGenerator]:
     # Now process the generator module
     # Remove the prefix and ".py" suffix
     relpath = source_paths.module_path.relative_to(source_paths.prefix)
-    relpath = str(relpath)
-    relpath = relpath[:-3]  # Remove ".py"
-    # relpath = str(source_paths.module_path)[len(str(source_paths.prefix)) + 1 : -3]
+    relpath = str(relpath)[:-3]  # Remove ".py"
     # And if it's a package, remove "__init__.py"
     if relpath.endswith("/__init__"):
         relpath = relpath[: -1 * len("/__init__")]
@@ -217,14 +246,6 @@ def try_candidate(source_paths: SourcePaths) -> Optional[SchematicGenerator]:
     except Exception as e:
         print(f"ERROR IMPORTING {modpath}: {e}")
         return None
-
-    # gps = SchematicGeneratorPaths(
-    #     lib_name=lib_name,
-    #     cell_name=cell_name,
-    #     module_path=path,
-    #     sch_path=sch_yaml_path,
-    # )
-    # open("paths.json", "a").write(json.dumps(gps, default=pydantic_encoder) + ",")
 
     # Create the generator, cache and return it
     gen = SchematicGenerator(source_paths=source_paths, pymodule=pymodule, sch=sch)
@@ -295,49 +316,124 @@ def order_helper(sch: BagSchematic, order: List[BagSchematic], seen: Set[LibCell
     order.append(sch)
 
 
-# for src in walk():
-#     pass
-#     # print(sch)
-#     # for cls in find_bag_modules(mod):
-#     #     print(cls.__qualname__)
-#     #     print(inspect.signature(cls.design))
-
-# paths = json.load(open("paths.json", "r"))
-# paths = [SchematicGeneratorPaths(**path) for path in paths]
-# paths = {(path.lib_name, path.cell_name): path for path in paths}
-
-# accum = list()
-# for path in paths.values():
-#     sch = get_sch(LibCell(path.lib_name, path.cell_name))
-#     order_helper(sch, accum)
-# print(f"NOT_FOUND: {not_found}")
-# print(f"ORDER: {accum}")
-# if not_found:
-#     raise TabError()
-# open("order.json", "w").write(json.dumps(accum))
-
-# order = json.load(open("order.json", "r"))
-# ordered_paths = [paths[tuple(k)] for k in order if tuple(k) not in prim_cells]
-# open("ordered_paths.json", "w").write(json.dumps(ordered_paths, default=pydantic_encoder))
+@dataclass(frozen=True)
+class SchematicGeneratorPaths:
+    # FIXME: deprecate
+    lib_name: str
+    cell_name: str
+    module_path: Path
+    sch_path: Path
 
 
 def ordered_stuff():
     """
-    Scratch-space, looking at ordered cells and their dependencies
+    Scratch-space
+    Looking at ordered cells and their dependencies
+    Lots of file-side caching of intermediate results being debugged in here,
+    especially after some long searches over the file system.
     """
-    ordered = json.load(open("ordered_paths.json", "r"))
-    for paths in ordered:
-        p = SchematicGeneratorPaths(**paths)
-        sch = load_sch(p.sch_path)
-        print(sch.cell_name)
-        # for name, term in sch.terminals.items():
-        #     inst = BagSchematicInstance(**term["obj"][1]["inst"])
-        #     # print(name)
-        #     # print(inst)
-        print(f"({(sch.lib_name, sch.cell_name)}) depends on:")
-        for inst in sch.instances.values():
-            print(f"     {(inst.lib_name, inst.cell_name)}")
+    # paths = json.load(open("paths.json", "r"))
+    # paths = [SchematicGeneratorPaths(**path) for path in paths]
+    # paths = {(path.lib_name, path.cell_name): path for path in paths}
 
+    # accum = list()
+    # for path in paths.values():
+    #     sch = get_sch(LibCell(path.lib_name, path.cell_name))
+    #     order_helper(sch, accum)
+    # print(f"NOT_FOUND: {not_found}")
+    # print(f"ORDER: {accum}")
+    # if not_found:
+    #     raise TabError()
+    # open("order.json", "w").write(json.dumps(accum))
+
+    # order = json.load(open("order.json", "r"))
+    # ordered_paths = [paths[tuple(k)] for k in order if tuple(k) not in prim_cells]
+    # open("ordered_paths.json", "w").write(json.dumps(ordered_paths, default=pydantic_encoder))
+
+    ordered = json.load(open("ordered_paths.json", "r"))
+    ordered = [SchematicGeneratorPaths(**paths) for paths in ordered]
+
+    for p in ordered:
+        sch = load_sch(p.sch_path)
+        print(p)
+        print_schematic_stuff(sch)
+
+def print_schematic_stuff(sch: BagSchematic):
+    """# Print some fun facts about schematic `sch`. """
+
+    print(f"({(sch.lib_name, sch.cell_name)})")
+
+    terminal_names = set(sch.terminals.keys())
+    print(terminal_names)
+
+    for name, term in sch.terminals.items():
+        print(parse_instance_or_port_name(name))
+        print(f"  {name} {term.inner.direction}")
+
+    print(f"depends on:")
+    for inst in sch.instances.values():
+        print(f"     {(inst.lib_name, inst.cell_name)}")
+        for portname, signame in inst.connections.items():
+            if signame not in terminal_names:
+                print(f"Internal signal: {signame}")
+
+
+@dataclass
+class Something:
+    """# Result of parsing and converting the maybe-scalar, maybe-bus names such as `i0<3:0>`.
+    These names are used for schematic instances and terminals (ports) to indicate their widths. 
+    FIXME: what should this be called? Not `Something`!"""
+
+    name: str
+    width: int
+
+
+def parse_instance_or_port_name(name: str) -> Something:
+    """
+    Instance and port names use something like this format:
+    * i0 == single instance
+    * i1<3:0> == array of four
+
+    Connection names also "concatenate" with commas,
+    and "replicate" with <*N> type notation,
+    which (we think?) isn't allowed here.
+    But check for it, and fail if it happens.
+    """
+    _fail = lambda: fail(f"Invalid instance or port name {name}")
+
+    if "," in name or "*" in name:
+        _fail()
+
+    if "<" not in name: # Scalar case 
+        return Something(name=name, width=1)
+
+    # OK now the fun part, parsing apart name and width.
+    # Format: `name<width:0>`
+    # The angle-bracket part must be at the end, and the low-side index must be zero. Or fail. 
+    split = name.split("<")
+    if len(split) != 2:
+        _fail()
+    name, suffix = split[0], split[1]
+    if not suffix.endswith(">"):
+        _fail()
+    suffix = suffix[:-1]  # Strip out the ending ">"
+    split = suffix.split(":")
+    if len(split) != 2:
+        _fail()
+    if split[1] != "0":
+        _fail()
+    try:  # Convert the leading section to an integer
+        width = int(split[0])
+    except:
+        fail(f"Could not covert `{split[0]}` to int in `{name}`")
+
+    # Success
+    return Something(name=name, width=width)
+
+@dataclass 
+class Repeat:
+    
+def parse_connection(conn: str) -> Something:
 
 def main():
     # Step 1: look for candidate python-module / schematic-YAML pairs
@@ -355,5 +451,11 @@ def main():
     # Step 6: Profit(?)
 
 
+def fail(msg: str):
+    """Error helper. Great place to stick a breakpoint."""
+    raise RuntimeError(msg)
+
+
 if __name__ == "__main__":
-    main()
+    ordered_stuff()
+    # main()
